7bc93bda59b4fe9f26f8a7ee6ccb24c8727461e8
Robert 'phaylon' Sedlacek <rs@474.at>
2009-08-22 00:12:56 +0200

    bumped version

--- lib/CatalystX/Declare.pm
+++ lib/CatalystX/Declare.pm
@@ -10,7 +10,7 @@ class CatalystX::Declare extends MooseX::Declare is dirty {
 
     clean;
 
-    our $VERSION = '0.009';
+    our $VERSION = '0.010';
 
     around keywords (ClassName $self:) {
         $self->$orig,
